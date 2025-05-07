92f7573b46ff2b3c6e0a19efe82ec95ed84e4a3b
PatrickLeonardo <patricklf7@proton.me>
2025-05-06 23:50:55 -0300

    create database connection

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,12 @@
+import java.sql.SQLException;
 
 import javax.swing.SwingUtilities;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
 
         SwingUtilities.invokeLater(() -> {
-            TelaLogin.criarTela();
+            new TelaLogin().criarTela();
         });
 
     }
